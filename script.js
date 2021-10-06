4045200273db79d35e704e466155cf0bb4534ce3
knktrang93 <87623875+knktrang93@users.noreply.github.com>
2021-10-06 15:38:06 -0700

    more adjusting

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 'use strict';
 
-const hiddenNumber = Math.trunc(Math.random() * 20 + 1);
+let hiddenNumber = Math.trunc(Math.random() * 20 + 1);
 let chances = 20;
+let highScore = 0;
 
 // conditions at check button
 document.querySelector('.check-btn').addEventListener('click', function () {
   const guessNumber = Number(document.querySelector('.guess-number').value);
+
   //if there is no input
   if (!guessNumber) {
     document.querySelector('.comment').textContent = 'Input a number below! ⬇';
-  } //if input equals secret number
+  }
+  //if input equals secret number
   else if (guessNumber === hiddenNumber) {
     document.querySelector('.comment').textContent =
       "Congratulations! You've guessed the secret number!";
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.hidden-number').textContent = hiddenNumber;
-  } //if input is higher than secret number
+
+    if (chances > highScore) {
+      highScore = chances;
+      document.querySelector('.high-score').textContent = chances;
+    }
+  }
+  //if input is higher than secret number
   else if (guessNumber > hiddenNumber) {
     if (chances > 1) {
       document.querySelector('.comment').textContent = 'Too high!';
@@ -25,7 +34,8 @@ document.querySelector('.check-btn').addEventListener('click', function () {
       document.querySelector('.chances').textContent = 0;
       document.querySelector('.comment').textContent = 'You lost! 😔 ';
     }
-  } //if input is lower than secret number
+  }
+  //if input is lower than secret number
   else if (guessNumber < hiddenNumber) {
     if (chances > 1) {
       document.querySelector('.comment').textContent = 'Too low!';
@@ -38,9 +48,10 @@ document.querySelector('.check-btn').addEventListener('click', function () {
   }
 });
 
+// new game function
 document.querySelector('.new-game').addEventListener('click', function () {
-  const hiddenNumber = Math.trunc(Math.random() * 20 + 1);
-  let chances = 20;
+  hiddenNumber = Math.trunc(Math.random() * 20 + 1);
+  chances = 20;
   document.querySelector('.hidden-number').textContent = '?';
   document.querySelector('.chances').textContent = chances;
   document.querySelector('.comment').textContent = 'Start guessing! ⬇';
